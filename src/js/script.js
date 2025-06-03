4999c685b75a889fd6e867bae41e577bce4170d5
giovanni <giovannirprovazi@gmail.com>
2025-06-03 11:40:54 -0300

    Criando hero session da landing page

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const clicarMenu = document.getElementById("menu-hamburguer")
+const menu = document.getElementById("menu")
+
+clicarMenu.addEventListener("click", () => {
+    menu.classList.toggle('hidden')
+})
